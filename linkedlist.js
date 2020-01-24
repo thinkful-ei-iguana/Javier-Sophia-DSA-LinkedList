cbc5d92f4c3f34d96e70fa5c0f79f170702168e5
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-01-24 14:58:44 -0500

    question 6: 3rd from end

--- linkedlist.js
+++ linkedlist.js
@@ -10,64 +10,135 @@ class LinkedList {
       this.head = null;
     }
   
+    insertFirst(item) {
+      this.head = new _Node(item, this.head);
+    }
+  
+    insertLast(item) {
+      if(this.head === null) {
+        this.insertFirst(item);
+      } else {
+        let tempNode = this.head;
+        while(tempNode.next !== null) {
+          tempNode = tempNode.next;
+        }
+        tempNode.next = new _Node(item, null);
+      }
+    }
+  
+    insertBefore(item, itemBefore){
+      let newNode = new _Node(item, null);
+      if(!this.head){
+        return null;
+      }
+  
+      if(this.head.value === itemBefore){
+        newNode.next = this.head;
+        this.head = newNode;
+        return;
+      }
+  
+      
+      let current = this.head; 
+      let prev = this.head;  
+  
+      while(current !== null){
+        if(current.value === itemBefore){
+          newNode.next = current;
+          prev.next = newNode;
+          return;
+        }
+        prev = current; 
+        current = current.next; 
+      }
+  
+      console.log(`${itemBefore} not found`);
+      return;
+    }
+  
+    insertAfter(item, itemAfter) {
+      const newNode = new _Node(item, null);
+      if (!this.head) {
+        return null;
+      } else {
+        let current = this.head;
+        while ((current.value !== itemAfter) && (current !== null)) {
+          current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+      }
+    }
+  
+    insertAt(item, index){
+      let newNode = new _Node(item, null);
+      if(index < 0){
+        console.log('index out of bounds');
+        return;
+      }
+      if(index === 0){
+        newNode.next = this.head;
+        this.head = newNode;
+        return;
+      }
+      
+      
+      let prev = this.head;
+      let current = this.head;
+      for(let i =0; i < index; i++){
+        if(current === null){
+          console.log('index out of bounds');
+          return;
+        }
+        prev = current;
+        current = current.next;
+      }
+  
+      newNode.next = current;
+      prev.next = newNode;
+    }
+  
     find(item) {
+      let currNode = this.head;
+  
       if (!this.head) {
         return null;
       }
-      let currNode = this.head;
   
       while (currNode.value !== item) {
-        if (currNode.next !== null) {
-          return null; //couldn't find item
+        if (currNode.next === null) {
+          return null;
         } else {
-          currNode = this.next;
-        }
-      }
-      
-      return currNode;
-    }
-  
-    insertFirst(value) {
-      //point to the head since it is either null or the first element
-      this.head = new _Node(value, this.head);
-    }
-  
-    insertLast(value) {
-      if (this.head === null) {
-        this.insertFirst(value);
-      } else {
-        let currNode = this.head;
-        while (currNode.next !== null) {
           currNode = currNode.next;
         }
-        //set the last node's next
-        currNode.next = new _Node(value, null);
       }
+  
+      return currNode;
     }
   
     remove(item) {
       if (!this.head) {
         return null;
       }
-      //if it is the first one
+  
       if (this.head.value === item) {
         this.head = this.head.next;
         return;
       }
-      //all others
+  
       let currNode = this.head;
       let previousNode = this.head;
   
-      while ((currNode !== null) && (currNode.value !== item)) {
+      while((currNode !== null) && (currNode.value !== item)) {
         previousNode = currNode;
         currNode = currNode.next;
       }
+  
       if (currNode === null) {
-        console.log("Item not found");
+        console.log('Item not found');
         return;
-      } else {
-        previousNode.next = currNode.next;
       }
+      previousNode.next = currNode.next;
     }
   }
 
@@ -84,6 +155,104 @@ function main(){
     sll.insertLast("Starbuck")
     sll.insertLast("Tauhida")
     sll.remove("squirrel")
-    console.log(JSON.stringify(sll));}
+    console.log(JSON.stringify(sll));
+}function ListSize(list){
+    let count = 0;
+    let current = SLL.head;
+    while(current != null){
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+  
+  function isEmpty(list){
+    if(list.head === null){
+      return true;
+    }
+    return false;
+  }
+  
+  function findPrev(list, item){
+    
+    let current = list.head;
+    let prev = list.head;
+  
+    while(current !== null){
+      if(current.value === item){
+        return prev.value;
+      }
+      prev = current; 
+      current = current.next;
+    }
+  }
+  
+  function findLast(list){
+    let current = list.head;
+  
+    while(current !== null){
+      if(current.next === null){
+        return current;
+      }
+      current = current.next;
+    }
+  }
+  
+  function reverseList(list) {
+    const newList = new LinkedList;
+    if(list.head === null) {
+      return null;
+    } else {
+      let currNode = list.head;
+      while(currNode !== null) {
+        newList.insertFirst(currNode.value);
+        currNode = currNode.next;
+      }
+    }
+    console.log(display(newList));
+    return newList;
+  }
+  
+  function thrdFromEnd(list){
+    let current = list.head;
+    let prev = list.head;
+    let res = list.head;
+    while(current.next !== null){
+      res = prev;
+      prev = current;
+      current = current.next;
+    }
+    return res.value;
+  }
+  
+  //console.log(display(SLL));
+  //console.log(ListSize(SLL));
+  //console.log(isEmpty(SLL));
+  //console.log(findPrev(SLL, 'Apollo'));
+  //console.log(findLast(SLL));
+  //reverseList(SLL);
+  //console.log(thrdFromEnd(SLL));
+  
+  
+  /*
+  REMOVES ANY DUPLICATES IN AN ARRAYLIST
+  TIME COMPLEXITY OF O(n^2)
+  function WhatDoesThisProgramDo(lst) {
+      let current = lst.head; 1
+      while (current !== null) n {
+          let newNode = current; 1 
+          while (newNode.next !== null) n {
+              if (newNode.next.value === current.value) { 1
+                  newNode.next = newNode.next.next; 1
+              }
+              else {
+                  newNode = newNode.next; 1
+              }
+          }
+          current = current.next;
+      }
+  }
+  */
+  
 
 main()
